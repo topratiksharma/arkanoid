25df6efd9f203d2ff8fe266feb0dacddfd1516ec
Pratik Sharma <pratiksharma@live.in>
2023-05-16 17:15:52 +0530

    commit

--- src/app/arkanoid/arkanoid.component.ts
+++ src/app/arkanoid/arkanoid.component.ts
@@ -1,16 +1,21 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+} from '@angular/core';
 import { Arkanoid } from '../helper-classes/arkanoid';
 import { Boundaries, ControlState } from '../types/types';
-import { CONFIG, CONTROLS } from './constants';
+import { CONFIG, CONTROLS } from './contants';
 
 @Component({
   selector: 'arkanoid',
   templateUrl: './arkanoid.component.html',
-  styleUrls: ['./arkanoid.component.scss']
+  styleUrls: ['./arkanoid.component.scss'],
 })
 export class ArkanoidComponent implements AfterViewInit {
-
-  @ViewChild('gameCanvas') canvasElement!: ElementRef
+  @ViewChild('gameCanvas') canvasElement!: ElementRef;
 
   public width: number = 800;
   public height: number = 600;
@@ -20,8 +25,9 @@ export class ArkanoidComponent implements AfterViewInit {
   private ticksPerSecond: number = 60;
 
   private controlState: ControlState;
-  player2Score = 0;
-  player1Score = 0;
+  public player2Score = 0;
+  public player1Score = 0;
+  private interval: any;
 
   constructor() {
     this.arkanoidGame = new Arkanoid(this.height, this.width);
@@ -30,24 +36,43 @@ export class ArkanoidComponent implements AfterViewInit {
 
   public ngAfterViewInit(): void {
     this.context = this.canvasElement?.nativeElement.getContext('2d');
-    this.playGame();
+    // Draw background
+    this.context.fillStyle = CONFIG.BACKGROUND_COLOR;
+    this.context.fillRect(0, 0, this.width, this.height);
 
-    // Game model ticks 60 times per second. Doing this keeps same game speed
-    // on higher FPS environments.
+    // Draw player1 paddle
+    this.addPlayer1Panel();
+
+    // Draw player2 paddle
+    this.addPlayer2Panel();
   }
 
   public playGame() {
-    this.renderFrame()
-    setInterval(() => this.arkanoidGame.tick(this.controlState), 1 / this.ticksPerSecond);
+    this.renderFrame();
+    this.interval = setInterval(
+      () => this.arkanoidGame.tick(this.controlState),
+      1 / this.ticksPerSecond
+    );
+  }
+
+  public resetPlayground() {
+    location.reload();
   }
 
   private renderFrame(): void {
     // Only run if game still going
-    if (this.arkanoidGame.gameOver()) {
-      this.context.font = "30px Verdana";
-      this.context.fillText("Game Over!", 300, 300); // TODO: Add Over in middle
-      setTimeout(() => location.reload(), 500); // DO not reload after 500, instead only start after user play
-      return; // Add a user play and pause button - add a reset button.
+    if (this.arkanoidGame.checkScore() === 'left') {
+      this.player2Score++;
+      this.context.font = '30px Verdana';
+      this.context.fillText('Player 2 scored!', 250, 300); // TODO: Add Over in middle
+      clearInterval(this.interval);
+      return;
+    } else if (this.arkanoidGame.checkScore() === 'right') {
+      this.player1Score++;
+      this.context.font = '30px Verdana';
+      this.context.fillText('Player 1 scored!', 250, 300); // TODO: Add Over in middle
+      clearInterval(this.interval);
+      return;
     }
 
     // Draw background
@@ -92,14 +117,24 @@ export class ArkanoidComponent implements AfterViewInit {
     this.context.fillStyle = CONFIG.PLAYER_1.COLOR;
     let player1 = this.arkanoidGame.player1;
     const bounds: Boundaries = player1.getCollisionBoundaries();
-    this.context.fillRect(bounds.left, bounds.top, player1.getWidth(), player1.getHeight());
+    this.context.fillRect(
+      bounds.left,
+      bounds.top,
+      player1.getWidth(),
+      player1.getHeight()
+    );
   }
 
   private addPlayer2Panel() {
     this.context.fillStyle = CONFIG.PLAYER_2.COLOR;
     let player2 = this.arkanoidGame.player2;
     const bounds: Boundaries = player2.getCollisionBoundaries();
-    this.context.fillRect(bounds.left, bounds.top, player2.getWidth(), player2.getHeight());
+    this.context.fillRect(
+      bounds.left,
+      bounds.top,
+      player2.getWidth(),
+      player2.getHeight()
+    );
   }
 
   private addBall() {
@@ -109,9 +144,5 @@ export class ArkanoidComponent implements AfterViewInit {
     this.context.beginPath();
     this.context.arc(bounds.left, bounds.top, 10, 0, 2 * Math.PI);
     this.context.fill();
-  }
-
-  public play() {
-    throw new Error('Method not implemented.');
   }
 }
