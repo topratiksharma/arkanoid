1ea2a9ca17a27a00d5396c20f01d1455598f2709
Pratik Sharma <pratiksharma@live.in>
2023-05-21 17:18:17 +0530

    instruction

--- src/app/shared/instructions/instructions.component.ts
+++ src/app/shared/instructions/instructions.component.ts
@@ -9,10 +9,10 @@ import {  MatDialogRef } from '@angular/material/dialog';
 export class InstructionsComponent {
 
   public rules = [
-    "Select game type by toggle switch.",
-    "Click on play button to begin the play.",
-    "Move your paddle up and down by pressing W(for up) and S(for down), in AI mode.",
-    "If playing 2 player mode, first players contros remain same, second player can move his paddle up and down via up and down arrow key.",
+    "Select the game type by toggle switch, Between AI Mode and two player.",
+    "Click on the play button to begin.",
+    "Move your paddle up and down by pressing W(for up) and S(for down) for player 1.",
+    "If playing 2 player mode, first players contros remain same, second player can move thier paddle up and down via up and down arrow key.",
     "Click on reset to re-start the game.",
   ]
   constructor(public dialogRef: MatDialogRef<InstructionsComponent>,
